713d3d6501171281795d85104d7cd9af1414c628
shekit <as7790@nyu.edu>
2017-09-20 18:54:29 -0400

    included webpack config files

---
+++ webpack.common.js
@@ -0,0 +1,55 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    app:'./src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module:{
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',  
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        //loaders processes in reverse order
+        use: [
+          'style-loader', // outputs css in embedded style tag in doc
+          'css-loader',   // parses css into javascript in bundle
+          'sass-loader'   // transforms sass into css
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000    // convert images <10k to base64 strings
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
---
+++ webpack.dev.js
@@ -0,0 +1,44 @@
+// run using ./node_modules/.bin/webpack-dev-server --config webpack.dev.js
+// To generate files: ./node_modules/.bin/webpack --config webpack.dev.js
+
+const path = require('path')
+const webpack = require('webpack')
+const common = require('./webpack.common.js')
+const merge = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    port: 8080,
+    hot: true,
+    inline: true,
+  },
+  plugins: [
+
+    // generates html file with all webpack bundles included in script tag
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+      favicon: path.resolve(__dirname, 'assets/images/favicon.ico')
+    }),
+
+    //prevents webpack CLI from stopping due to errors 
+    new webpack.NoEmitOnErrorsPlugin(),
+
+    //improve logging in the console
+    new webpack.NamedModulesPlugin(),
+
+    // see changes without reloading browser
+    new webpack.HotModuleReplacementPlugin(),
+
+    // passes variables from webpack to js files
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV':JSON.stringify('development')
+      }
+    })
+
+  ]
+})
