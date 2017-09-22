45d54e26f06c5fd3ed963a19d9d59d8a912ba028
Tal Ater <tal@talater.com>
2017-09-22 11:42:26 +0200

    By using an arrow function here you can avoid using hacks such as `self = this`

--- src/components/presentationals/Badge.jsx
+++ src/components/presentationals/Badge.jsx
@@ -4,10 +4,9 @@ import PropTypes from 'prop-types'
 class Badge extends Component {
 
   render(){
-    var self = this;
-    var badgeList = this.props.stateProps.badges.map(function(badgeId, index){
-      return (<li key={index}>{self.props.stateProps.badgesById[badgeId].title}</li>)
-    })
+    var badgeList = this.props.stateProps.badges.map((badgeId, index) =>
+      (<li key={index}>{this.props.stateProps.badgesById[badgeId].title}</li>)
+    );
     return(
       <div>
         <ul>{badgeList}</ul>
