4b790bfb425f294d19f8c720d4355d431165953f
shekit <as7790@nyu.edu>
2017-09-23 00:49:14 -0400

    code cleanup, implemented connect and redux action for badges

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import {Footer} from './components/presentationals/Footer.jsx';
 import BadgesContainer from './components/containers/BadgesContainer.jsx';
-import PropTypes from 'prop-types';
-import store from './store';
 
 class App extends Component {
   render(){
--- src/components/containers/BadgesContainer.jsx
+++ src/components/containers/BadgesContainer.jsx
@@ -1,30 +1,8 @@
 // connect store state and action creators to react component props
 
-import React, {Component} from 'react';
-//import {getState} from 'redux'
-
 import {connect} from 'react-redux';
 import {addBadge} from '../../actions';
-
 import Badges from '../presentationals/Badges.jsx';
-
-/*import PropTypes from 'prop-types';
-
-class BadgeContainer extends Component {
-  render(){
-    const stateProps = this.props.store.getState();
-    return (
-      <div>
-        <h2>Badges</h2>
-        <Badges stateProps = {stateProps} />
-      </div>
-    );
-  }
-}
-
-BadgeContainer.propTypes = {
-  store: PropTypes.object
-};*/
 
 const mapDispatchToProps = (dispatch) => ({
   addBadge: () => dispatch(addBadge())
@@ -36,5 +14,3 @@ const mapStateToProps = (state) => ({
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Badges);
-
-//export default BadgeContainer;
