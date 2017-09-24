4761f9c09917fec405de31ac3249cdf23e4a0b04
shekit <as7790@nyu.edu>
2017-09-24 01:21:34 -0400

    added placeholder actions for badges

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,3 +3,13 @@ import {createAction} from 'redux-actions';
 export const addBadge = createAction('ADD_BADGE');
 
 export const addBadgeAsync = createAction('ADD_BADGE_ASYNC');
+
+export const getUserBadges = createAction('GET_USER_BADGES');
+
+export const getUserBadgesSuccess = createAction('GET_USER_BADGES_SUCCESS');
+
+export const getUserBadgesError = createAction('GET_USER_BADGES_ERROR');
+
+export const addUserBadge = createAction('ADD_USER_BADGE');
+
+export const removeUserBadge = createAction('REMOVE_USER_BADGE');
