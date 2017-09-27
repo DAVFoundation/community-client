187921e7af528885f7c5740f84a3ffe6c2aeda37
shekit <as7790@nyu.edu>
2017-09-26 23:28:32 -0400

    added sagas to get user badges

--- src/sagas/badgeSagas.js
+++ src/sagas/badgeSagas.js
@@ -1,12 +1,18 @@
 import {delay} from 'redux-saga';
-import {put,call,takeEvery} from 'redux-saga/effects';
-import {addBadge, addBadgeAsync} from '../actions';
+import {put,call,takeEvery, takeLatest} from 'redux-saga/effects';
+import {addBadge, addBadgeAsync, getUserBadges, getUserBadgesSuccess, getUserBadgesError} from '../actions';
 
 // WATCHER SAGAS
 export function* watchAddBadge() {
   console.log("watching for add badge");
   yield takeEvery(addBadgeAsync, workerAddBadge);
-} 
+}
+
+export function* watchGetUserBadges(){
+  yield takeLatest(getUserBadges, workerGetUserBadges);
+}
+
+
 
 // WORKER SAGAS
 export function* workerAddBadge(){
@@ -14,3 +20,46 @@ export function* workerAddBadge(){
   yield call(delay, 1000);
   yield put(addBadge(2));
 }
+
+export function* workerGetUserBadges(){
+  try{
+    const resp = yield call(badgeApi);
+    console.log(resp);
+
+    yield put(getUserBadgesSuccess(resp));
+  } catch(error){
+    yield put(getUserBadgesError(error));
+  }
+}
+
+function badgeApi(){
+  return fetch("http://localhost:3000/api/badges", {
+    method: 'GET'
+  })
+    .then(handleApiErrors)
+    .then(resp => resp.json())
+    .then(json => json)
+    .catch((error) => {throw error;});
+
+  /*return {
+    badgeIds : [3,5],
+    badgesById : {
+      3 : {
+        id: 3,
+        img: "founder",
+        title: "API Founder"
+      },
+      5 : {
+        id: 5,
+        img : "contributor",
+        title: "API Contributor"
+      }
+    }
+  };*/
+}
+
+function handleApiErrors(resp){
+  if(!resp.ok) throw Error(resp.statusText);
+  return resp;
+}
+
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,12 @@
 import "regenerator-runtime/runtime";
 import {delay} from 'redux-saga';
 import {put,call, takeEvery, all} from 'redux-saga/effects';
-import {watchAddBadge} from './badgeSagas';
+import {watchAddBadge, watchGetUserBadges} from './badgeSagas';
 
 // register watcher sagas
 export default function* rootSaga(){
   yield all([
-    watchAddBadge()
+    watchAddBadge(),
+    watchGetUserBadges()
   ]);
 }
