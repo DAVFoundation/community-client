0e4b7501e6120a6e8810b3c09556bb72550e8e2e
shekit <as7790@nyu.edu>
2017-09-30 17:33:54 -0400

    combined user related reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import badges from './badges';
 import newsfeed from './newsfeed';
+import user from './user';
 
 export default combineReducers({
+  user,
   badges,
   newsfeed
 });
