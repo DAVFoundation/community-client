eacf58707bd0190500797bd48a410375ec6ffb5d
shekit <as7790@nyu.edu>
2017-10-04 03:31:23 -0400

    added update styling, update details and image placeholder

--- src/components/presentationals/Updates.jsx
+++ src/components/presentationals/Updates.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import '../../static/css/Updates.css';
 
 class Updates extends Component {
 
@@ -19,7 +20,7 @@ class Updates extends Component {
 
     return(
       <div>
-        <ul className='list-group'>{updatelist}</ul>
+        <ul id="update-list" className='list-group'>{updatelist}</ul>
       </div>
     );
   }
@@ -32,8 +33,18 @@ class UpdateItem extends Component {
   }
 
   render(){
+    var d = new Date(this.props.details.createdAt);
     return (
-      <li className='list-group-item'>{this.props.details.description}</li>
+      <li className='list-group-item'>
+        <div className="update-image">
+
+        </div>
+        <div className="update-user">
+          <h5 className="update-title">{this.props.details.name}</h5>
+          <p className="update-date">{d.toUTCString()}</p>
+        </div>
+        <p className="update-description">{this.props.details.description}</p>
+      </li>
     );
   }
 }
