b61105c08a51f57d5a8bfc8c47925fd87a63a4ac
shekit <as7790@nyu.edu>
2017-10-11 11:59:32 -0400

    added action to submit form and validate

--- src/components/containers/FormContainer.jsx
+++ src/components/containers/FormContainer.jsx
@@ -1,19 +1,23 @@
 import {connect} from 'react-redux';
 import {reduxForm} from 'redux-form';
-import Form from '../presentationals/Form.jsx';
-import {addBadge} from '../../actions';
+import Form, {validate} from '../presentationals/Form.jsx';
+import {submitStationForm} from '../../actions';
+
 
 const mapStateToProps = (state) => ({
   formType: state.modals.modalProps.tab,
-  initialValues: state.modals.modalProps.tab
+  initialValues: state.modals.modalProps.tab,
+  form: state.form //populated by redux form
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  addBadge: (val) => dispatch(addBadge(val))
+  submitStationForm: (obj) => dispatch(submitStationForm(obj)),
+
 });
 
 const Comp = connect(mapStateToProps, mapDispatchToProps)(Form);
 
 export default reduxForm({
-  form: 'station-form'//unique form name
+  form: 'station-form',//unique form name or tag, for form related function like clearing or resetting form
+  validate
 })(Comp);
--- src/components/presentationals/Form.jsx
+++ src/components/presentationals/Form.jsx
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {Field} from 'redux-form';
+import {Field, reduxForm, SubmissionError} from 'redux-form';
 
 class Form extends Component {
   constructor(props){
     super(props);
-    this.submitForm = this.submitForm.bind(this);
+    this.submit = this.submit.bind(this);
   }
 
-  submitForm(values){
+  submit(values){
     let edited = Object.assign({},values, {
       type: this.props.formType
     });
     console.log(edited);
+
+    return new Promise((resolve, reject) => {
+      //dispatch action
+      this.props.submitStationForm({edited, resolve, reject});
+    }).catch(error => {
+      throw new SubmissionError(error);
+    });
+  }
+
+  customField({input,label, meta: {touched, error}, ...custom}){
+    const hasError = touched && error !== undefined;
+    console.log(touched);
+    console.log(error);
+    console.log(hasError);
+    return(
+      <div>
+        {hasError && "ERROR"}
+      </div>
+    );
   }
 
   render(){
@@ -37,15 +56,14 @@ class Form extends Component {
 
 
     return(
-      <form onSubmit={this.props.handleSubmit(this.submitForm)}>
+      <form onSubmit={this.props.handleSubmit(this.submit)}>
         <Field name="address" component="input" placeholder="Your Address" className="form-control"/>
+        <Field name="custom" component={this.customField} />
         <Field name="residenceType" className="form-control" component="select">
-          <option />
-          <option value="Business">Business Residence</option>
           <option value="Private">Private Residence</option>
+          <option value="Business">Business Residence</option>
         </Field>
         <Field name="electricalOutlet" className="form-control" component="select">
-          <option />
           <option value="<10m">&lt;10m</option>
           <option value="10-20">10m-20m</option>
           <option value=">20m">&gt;20m</option>
@@ -58,9 +76,20 @@ class Form extends Component {
   }
 }
 
+export const validate = (values) => {
+  const errors = {};
+  if(!values.address || values.address.trim() == ''){
+    errors.custom = 'Address Required'; // name should be same as form field name
+  }
+  console.log(errors);
+  return errors;
+};
+
 Form.propTypes = {
   handleSubmit : PropTypes.func,
-  formType: PropTypes.string
+  formType: PropTypes.string,
+  submitStationForm: PropTypes.func,
+  reset: PropTypes.func
 };
 
 export default Form;
