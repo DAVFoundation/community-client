25b4dc82b4f447bbc83611101aceb6a90283e95f
shekit <as7790@nyu.edu>
2017-10-12 01:45:40 -0400

    added authentication to state

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,11 +1,14 @@
 import {handleActions} from 'redux-actions';
 import {getUserSuccess, getUserError} from '../actions';
 
-const initialState = {};
+const initialState = {
+  authenticated: false
+};
 
 export default handleActions({
   [getUserSuccess]: (state, action) => {
     let edited = Object.assign({}, state, action.payload);
+    edited.authenticated = true;
     return edited;
   },
   [getUserError]: (state, action) => {
