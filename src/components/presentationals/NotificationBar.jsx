da990977d0def6f4d96f609c0dce7c20cdd59d1e
shekit <as7790@nyu.edu>
2017-10-13 01:40:41 -0400

    added close button

--- src/components/presentationals/NotificationBar.jsx
+++ src/components/presentationals/NotificationBar.jsx
@@ -3,14 +3,28 @@ import PropTypes from 'prop-types';
 import '../../static/css/NotificationBar.css';
 
 class NotificationBar extends Component {
+
   constructor(props){
     super(props);
+    this.hideNot = this.hideNot.bind(this);
+  }
+
+  hideNot(e){
+    e.preventDefault();
+    this.props.hideNotification();
   }
 
   render(){
+    if(!this.props.visible){
+      return null;
+    }
+
     return(
       <div id="notification-bar" className="col-12">
         <p>{this.props.description}</p>
+        <a onClick={this.props.hideNotification}>
+          <span className="close">&times;</span>
+        </a>
       </div>
     );
   }
@@ -19,7 +33,9 @@ class NotificationBar extends Component {
 NotificationBar.propTypes = {
   title: PropTypes.string,
   description: PropTypes.string,
-  action: PropTypes.string
+  action: PropTypes.string,
+  visible: PropTypes.bool,
+  hideNotification: PropTypes.func
 };
 
 export default NotificationBar;
