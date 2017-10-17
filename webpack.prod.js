bdfa99ffe96b275e5edf56b284626a91e1d3b75e
Tal Ater <tal@talater.com>
2017-10-17 21:50:08 +0300

    Added webpack.prod.js and change build script to use it
    `webpack.prod.js` is still a 1:1 duplicate of `webpack.dev.js `

---
+++ webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const webpack = require('webpack');
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src'),
+    port: 8080,
+    hot: true,
+    inline: true,
+    //public: 'localhost.com'
+  },
+  plugins: [
+
+    // generates html file with all webpack bundles included in script tag
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'src/index.html'),
+      favicon: path.resolve(__dirname, 'src/static/images/favicon.ico'),
+      inject: 'body'
+    }),
+
+    //prevents webpack CLI from stopping due to errors
+    new webpack.NoEmitOnErrorsPlugin(),
+
+    //improve logging in the console
+    new webpack.NamedModulesPlugin(),
+
+    // see changes without reloading browser
+    new webpack.HotModuleReplacementPlugin(),
+
+    // passes variables from webpack to js files
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV':JSON.stringify('development')
+      }
+    })
+
+  ],
+  externals: {
+    config: JSON.stringify(require("./src/config/config.dev.js"))
+  }
+})
