612c8a071e2d948f7706db9e4c686e1a7d9c0373
Tal Ater <tal@talater.com>
2017-10-29 10:30:23 +0200

    Added new favicon

--- webpack.dev.js
+++ webpack.dev.js
@@ -18,7 +18,7 @@ module.exports = merge(common, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: path.resolve(__dirname, 'src/index.html'),
-      favicon: path.resolve(__dirname, 'src/static/images/favicon.ico'),
+      favicon: path.resolve(__dirname, 'src/static/images/favicon.png'),
       inject: 'body'
     }),
 
