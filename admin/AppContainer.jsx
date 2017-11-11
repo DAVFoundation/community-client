b3cb080d0a066f1412ee7041cf925834a357e87e
shekit <as7790@nyu.edu>
2017-11-10 22:06:03 -0500

    pass admin status as props

--- admin/AppContainer.jsx
+++ admin/AppContainer.jsx
@@ -2,7 +2,8 @@ import {connect} from 'react-redux';
 import App from './App.jsx';
 
 const mapStateToProps = (state) => ({
-  authenticated: state.user.authenticated
+  authenticated: state.user.authenticated,
+  canAccessAdmin: state.user.permissions.canAccessAdmin
 });
 
 export default connect(mapStateToProps)(App);
