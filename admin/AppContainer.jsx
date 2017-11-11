6b648c88808388a666d0e1fd434969a838701d5b
shekit <as7790@nyu.edu>
2017-11-10 22:10:53 -0500

    adjusted props

--- admin/AppContainer.jsx
+++ admin/AppContainer.jsx
@@ -2,8 +2,7 @@ import {connect} from 'react-redux';
 import App from './App.jsx';
 
 const mapStateToProps = (state) => ({
-  authenticated: state.user.authenticated,
-  canAccessAdmin: state.user.permissions.canAccessAdmin
+  authenticated: state.user.authenticated
 });
 
 export default connect(mapStateToProps)(App);
