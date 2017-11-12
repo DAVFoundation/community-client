52e6c0b3fc741c7191bfc6b79fe81b7105b1a313
shekit <as7790@nyu.edu>
2017-11-12 00:39:14 -0500

    added api calls to server

---
+++ admin/lib/api.js
@@ -0,0 +1,47 @@
+import config from '../config';
+import store from '../store';
+import {getDavUpdates} from '../actions';
+
+export function apiGetUser(){
+  return fetch(`${config.api.endpoint}/api/user`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+    .then(resp => {
+      if(!resp.ok) window.location.replace(config.login.endpoint);
+      return resp;
+    })
+    .then(resp => resp.json())
+    .then(json => json)
+    .catch(error => {throw error;});
+}
+
+export function apiLogoutUser(){
+  return fetch(`${config.api.endpoint}/api/logout`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+    .then(handleApiErrors)
+    .then(resp => resp.json())
+    .then(json => {
+      window.location.replace(config.login.endpoint)
+    })
+    .catch(error => {throw error;})
+}
+
+
+export function apiGetDavUpdates(){
+  return fetch(`${config.api.endpoint}/api/admin/updates/list`, {
+    method: 'GET',
+    credentials: 'include'
+  })
+    .then(handleApiErrors)
+    .then(resp => resp.json())
+    .then(json => json)
+    .catch(error=>{throw error;});
+}
+
+function handleApiErrors(resp){
+  if(!resp.ok) throw Error(resp.statusText);
+  return resp;
+}
