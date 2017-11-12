9778014d3e2d3aff81e9a26ce8478c229d59f9aa
shekit <as7790@nyu.edu>
2017-11-12 12:32:02 -0500

    corrected function naming error

--- admin/reducers/forms.js
+++ admin/reducers/forms.js
@@ -2,7 +2,8 @@ import {handleActions} from 'redux-actions';
 import {submitUpdateFormSuccess, submitUpdateFormError} from '../actions';
 
 const initialState = {
-
+  success: false,
+  error: false
 };
 
 export default handleActions({
@@ -11,7 +12,7 @@ export default handleActions({
     return edited;
   },
 
-  [submitStationFormError]: (state,action) => {
+  [submitUpdateFormError]: (state,action) => {
     let edited = Object.assign({}, state, action.payload);
     return edited;
   }
