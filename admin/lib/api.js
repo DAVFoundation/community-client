ce5b07d08af236fe452ca0a7f33563bc5731ebdc
shekit <as7790@nyu.edu>
2017-11-12 11:46:44 -0500

    added api call to add dav update

--- admin/lib/api.js
+++ admin/lib/api.js
@@ -1,4 +1,5 @@
 import config from '../config';
+import store from '../store';
 import {getDavUpdates} from '../actions';
 
 export function apiGetUser(){
@@ -28,6 +29,24 @@ export function apiLogoutUser(){
     .catch(error => {throw error;});
 }
 
+export function apiCreateDavUpdate(formData){
+  return fetch(`${config.api.endpoint}/api/admin/update/add`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Accept': '*/*',
+      'Content-Type': 'application/json',
+      'Cache': 'no-cache',
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(handleApiErrors)
+    .then(resp => resp.json())
+    .then(json => {
+      store.dispatch(getDavUpdates());
+    })
+    .catch(error => {throw error;});
+}
 
 export function apiGetDavUpdates(){
   return fetch(`${config.api.endpoint}/api/admin/update/list`, {
