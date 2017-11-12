24169589e5f032f553d611ec908932294a030189
shekit <as7790@nyu.edu>
2017-11-12 11:55:50 -0500

    added sagas for form submit

--- admin/sagas/formSagas.js
+++ admin/sagas/formSagas.js
@@ -1,3 +1,24 @@
 import {put, call, takeLatest} from 'redux-saga/effects';
 import {submitUpdateForm, submitUpdateFormSuccess, submitUpdateFormError} from '../actions';
 import {apiCreateDavUpdate} from '../lib/api';
+
+
+// WATCHER
+
+export function* watchUpdateFormSubmit(){
+  yield takeLatest(submitUpdateForm, workerUpdateFormSubmit);
+}
+
+// WORKER
+
+export function* workerUpdateFormSubmit(action){
+  try {
+    const formData = action.payload.values;
+    const resp = yield call(apiCreateDavUpdate, formData);
+    yield put(submitUpdateFormSuccess(resp));
+    yield call(action.payload.resolve);
+  } catch(error) {
+    yield put(submitUpdateFormError(error));
+    yield call(action.payload.reject);
+  }
+}
