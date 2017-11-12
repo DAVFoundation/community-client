e1c536e16513e04aeb3ead129dfe2dbc474b502f
shekit <as7790@nyu.edu>
2017-11-12 00:18:21 -0500

    added sagas as middleware

--- admin/store/index.js
+++ admin/store/index.js
@@ -1,9 +1,15 @@
 import {createStore, applyMiddleware} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import reducers from '../reducers';
+import rootSaga from '../sagas';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
-  reducers
+  reducers,
+  applyMiddleware(sagaMiddleware)
 );
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
