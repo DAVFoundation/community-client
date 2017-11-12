d8b4ade8c8e531eca197754d52db6f80edd88e58
shekit <as7790@nyu.edu>
2017-11-12 14:13:41 -0500

    added form and basic validation

--- admin/components/presentationals/UpdateForm.jsx
+++ admin/components/presentationals/UpdateForm.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Field, SubmissionError} from 'redux-form';
+import '../../static/css/UpdateForm.css';
 
 class UpdateForm extends Component {
 
@@ -9,7 +11,7 @@ class UpdateForm extends Component {
   }
 
   submit(values){
-    console.log("submit");
+    console.log(values);
   }
 
   handleKeyDown(e){
@@ -26,8 +28,15 @@ class UpdateForm extends Component {
     }
 
     return(
-      <div>
-        <h1>UPDATE FORM</h1>
+      <div className="col-md-6">
+        <h2 className="section-header">Add Update</h2>
+        <div id="form-wrapper">
+          <form onSubmit={this.props.handleSubmit(this.submit)} onKeyDown={(e) => {this.handleKeyDown(e);}}>
+            <Field name="description" className="form-control" component="textarea" type="text" placeholder="Description"/>
+            <Field name="link" className="form-control" component="input" type="text" placeholder="Link (optional)"/>
+            <button type="submit" disabled={this.props.submitting} className="btn btn-custom">Add Update</button>
+          </form>
+        </div>
       </div>
     );
   }
@@ -35,13 +44,26 @@ class UpdateForm extends Component {
 }
 
 export const validate = (values) => {
+  const errors = {};
+  errors.error = '';
 
+  if(!values.description || values.description.trim() == ''){
+    errors.error += 'Description required ';
+  }
+
+  if(!/(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-/]))?/i.test(values.link)){
+    errors.error += 'Invalid link ';
+  }
+  console.log(errors);
+  return errors;
 };
 
 UpdateForm.propTypes = {
   success: PropTypes.bool,
   error: PropTypes.bool,
+  submitting: PropTypes.bool,
   submitUpdateForm: PropTypes.func,
+  handleSubmit: PropTypes.func,
   permissions: PropTypes.object
 };
 
--- admin/components/presentationals/Updates.jsx
+++ admin/components/presentationals/Updates.jsx
@@ -8,21 +8,58 @@ class Updates extends Component {
   }
 
   componentDidMount(){
-
+    this.props.getDavUpdates();
   }
 
   render(){
+
+    let updateList = this.props.updates.map((update, index) => {
+      return (<UpdateItem key={index} details={update} />);
+    });
     return(
       <div>
-        <h2>Update List</h2>
+        <h2 className="section-header">Update List</h2>
+        <table className="table table-striped">
+          <thead>
+            <th scope="col">Date</th>
+            <th scope="col">Description</th>
+            <th scope="col">Actions</th>
+          </thead>
+          <tbody>
+            {updateList}
+          </tbody>
+        </table>
       </div>
     );
   }
 }
 
+class UpdateItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <tr>
+          <td>{this.props.details.createdAt}</td>
+          <td>{this.props.details.description}</td>
+          <td>Actions</td>
+        </tr>
+      </div>
+    );
+  }
+
+}
+
 Updates.propTypes = {
   updates: PropTypes.array,
   getDavUpdates: PropTypes.func
+};
+
+UpdateItem.propTypes = {
+  details: PropTypes.object
 };
 
 export default Updates;
