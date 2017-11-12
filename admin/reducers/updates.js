8ea282686fb2517db973f5f098ad90abc46f36ef
shekit <as7790@nyu.edu>
2017-11-12 15:32:30 -0500

    successfully deleting updates and updating list

--- admin/reducers/updates.js
+++ admin/reducers/updates.js
@@ -8,12 +8,16 @@ const initialState = {
 export default handleActions({
   [getDavUpdatesSuccess]: (state, action) => {
     let edited = Object.assign({}, state, action.payload);
+    console.log(edited);
     return edited;
   },
 
   [deleteDavUpdateSuccess]: (state, action) => {
-    console.log(action.payload);
-
-    return state;
+    let remainingUpdates = state.updates.filter((obj) => {
+      return obj._id != action.payload.id;
+    });
+    let edited = {};
+    edited.updates = remainingUpdates;
+    return edited;
   }
 }, initialState);
