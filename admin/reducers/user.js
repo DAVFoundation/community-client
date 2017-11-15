d0f23638c95d0b5a55e5e4d1b8b487a27d6a674a
shekit <as7790@nyu.edu>
2017-11-14 21:41:01 -0500

    added permissions to state

--- admin/reducers/user.js
+++ admin/reducers/user.js
@@ -2,13 +2,15 @@ import {handleActions} from 'redux-actions';
 import {getUserSuccess, getUserError} from '../actions';
 
 const initialState = {
-  authenticated: false
+  authenticated: false,
+  permissions: {}
 };
 
 export default handleActions({
   [getUserSuccess]: (state, action) => {
     let edited = Object.assign({}, state, action.payload);
     edited.authenticated = true;
+    edited.permissions = action.payload.permissions;
     console.log(edited);
     return edited;
   },
